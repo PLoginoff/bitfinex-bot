4438ce07d6721a6b5ad4fed3a2fa62217b8d420c
Pavel Loginov <ploginoff@gmail.com>
2020-06-17 17:37:03 +0300

    first version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bitfinex-bot
+
+ - copy config.php.dist to config.php and setup keys
+ - composer install
+ - php run.php
--- run.php
+++ run.php
@@ -1,9 +1,22 @@
 <?php
 
-require 'Bitfinex.php';
-$config = require 'config.php';
+require_once 'vendor/autoload.php';
 
-$client = new Bitfinex($config['key'], $config['secret']);
+use Lin\Bitfinex\Bitfinex;
 
-var_dump($client->get_balances());
-var_dump($client->submitOrder());
+$config   = require 'config.php';
+$bitfinex = new Bitfinex($config['key'], $config['secret']);
+
+//Place an Order
+try {
+    $result=$bitfinex->order()->postSubmit([
+        //'cid'=>'',
+        'type'      => 'LIMIT',
+        'symbol'    => 'tBTCUSD',
+        'price'     => '1000',
+        'amount'    => '0.001',//Amount of order (positive for buy, negative for sell)
+    ]);
+    print_r($result);
+}catch (\Exception $e){
+    print_r(json_decode($e->getMessage(),true));
+}
